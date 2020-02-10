f58442fd1b67077d53c465d7974984d6e6e43deb
Mokerstier <pannekoek-wouter@hotmail.com>
2020-02-10 13:02:05 +0100

    forgot to add variables

--- docs/js/modules/marvelCall.js
+++ docs/js/modules/marvelCall.js
@@ -1,3 +1,7 @@
+let userAPIKEY = `9f1dfce0c33d520203276ccf628a6c26`
+let url =  `https://gateway.marvel.com/v1/public/characters`
+const params = `?apikey=${userAPIKEY}`
+
 export function apiCall() {
   fetch(`${url}${params}`)
     .then((res) => {
